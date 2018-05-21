c991d5985932db1b35fa36b0ca08b798a56009b1
katrina <0993262626g@gmail.com>
2018-05-21 21:12:39 +0300

    add php

--- ruslana/russ.js
+++ ruslana/russ.js
@@ -14,4 +14,24 @@ var btn=document.querySelector('.buttoncl'),
 		if(e.target==modal){
 			modal.style.display='none';
 		}
-	})
+	});
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Заявка отправлена, мы свяжемся с Вами в течении 5 минут");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});

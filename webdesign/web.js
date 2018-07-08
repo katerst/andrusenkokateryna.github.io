fa63986615dfb76a6f3475c94dad4a72dd61528f
katrina <0993262626g@gmail.com>
2018-07-08 21:09:39 +0300

    icons float and some2

--- webdesign/web.js
+++ webdesign/web.js
@@ -4,9 +4,11 @@ $(function() {
 		var get_id=this.id;
 		var get_current=$('.posts .' + get_id);
 
+
 		$('.post').not(get_current).hide(500);
 		get_current.show(500);
 	});
+});
 
 	$('#showall').click(function() {
 		$('.post').show(500);
